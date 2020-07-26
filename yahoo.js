8082ff802705e47c52aeee6df4b0b6a78a52ddd2
Kentaro <47137668+keny1102@users.noreply.github.com>
2020-07-26 21:20:57 +0900

    Update yahoo.js

--- yahoo.js
+++ yahoo.js
@@ -3,18 +3,30 @@ var standalone = window.navigator.standalone,
     userAgent = window.navigator.userAgent.toLowerCase(),
     safari = /safari/.test( userAgent ),
     ios = /iphone|iPod|ipad/.test( userAgent );
+var text = null;
+var element = document.createElement('h1'); 
 
 if( ios ) {
     if ( !standalone && safari ) {
         console.log("browser");
+        text = "browser";
+        
     } else if ( standalone && !safari ) {
         console.log("standalone");
+        text = "standalone";
     } else if ( !standalone && !safari ) {
         console.log("uiwebview");
+        text ="uiwebview";
     };
 } else {
     console.log("not iOS");
+    text = "not iOS";
 };
+
+
+element.id = "id"; 
+element.innerHTML = "text"; 
+
 
     if (1==0){
          window.location.href = 'http://www.yahoo.co.jp/';
